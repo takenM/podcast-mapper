24fa2afa6714e6bb7ed3d7b382c5c7401d3b59b3
takenM <11594083+takenM@users.noreply.github.com>
2023-08-14 16:27:20 +0900

    add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic_auth[:user] && password == Rails.application.credentials.basic_auth[:pass]
+    end
+  end
 end
